54cd62413426329b7c72597a59e6a63202c7ca31
joxliu <joxliu@tencent.com>
2023-04-01 21:07:31 +0800

    rm vsc cnf

--- remove_large_git_file.sh
+++ remove_large_git_file.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-for i in $(git rev-list --objects --all | grep "$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -10 | awk '{print$1}')" | awk ' { print $2 }'); do
+for i in $(git rev-list --objects --all | grep "$(git verify-pack -v .git/objects/pack/*.idx | sort -k 6 -n | tail -20 | awk '{print$1}')" | awk ' { print $2 }'); do
     echo $i
     echo "git filter-branch --force --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch '$i'' --tag-name-filter cat -- --all "
     git filter-branch --force --prune-empty --index-filter 'git rm -rf --cached --ignore-unmatch '$i'' --tag-name-filter cat -- --all
